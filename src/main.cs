2c4cfb9e11963c09abf2e17a983647aba9c3601e
DESKTOP-OOGTVPM\mohameds <mohameds@99x.io>
2025-01-04 21:29:12 +0530

    stage one

--- src/main.cs
+++ src/main.cs
@@ -1,7 +1,7 @@
 using System.Net;
 using System.Net.Sockets;
 
-Uncomment this line to pass the first stage
+// Uncomment this line to pass the first stage
 Console.Write("$ ");
 
 // Wait for user input
