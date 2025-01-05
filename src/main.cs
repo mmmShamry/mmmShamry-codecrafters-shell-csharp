cc5be9d76fa438280f4e6837a8f51cbe6254f7cb
mmmShamry <mmmshamry@gmail.com>
2025-01-05 08:57:28 +0530

    stage seven

--- src/main.cs
+++ src/main.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Net;
 using System.Net.Sockets;
 
@@ -27,6 +28,11 @@ while (true)
             Console.WriteLine($"{command.Substring(5)} is a shell builtin");
             continue;
         }
+        else if(pathLookup(command.Substring(5), out string foundAt))
+        {
+            Console.WriteLine($"{command.Substring(5)} is {Path.Combine(foundAt, command.Substring(5))}");
+            continue;
+        }
         else
         {
             Console.WriteLine($"{command.Substring(5)}: not found");
@@ -38,4 +44,25 @@ while (true)
         Console.WriteLine($"{command}: command not found");
         continue;
     }
+}
+
+
+bool pathLookup (string cmd, [NotNullWhen(true)] out string foundAt)
+{
+    string? pathEnvVar = Environment.GetEnvironmentVariable("PATH");
+    if (pathEnvVar != null)
+    {
+        string[] parts = pathEnvVar.Split(Path.PathSeparator);
+        foreach (string part in parts)
+        {
+            if (File.Exists(Path.Combine(part, cmd)))
+            {
+                foundAt = part;
+                return true;
+            }
+        }
+    }
+
+    foundAt = null;
+    return false;
 }
